fb430989e61ad44dcea920db0f03ee7c75dbc2fa
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-08-14 15:50:58 -0300

    Cada tenis

--- src/controllers/shoes.controller.js
+++ src/controllers/shoes.controller.js
@@ -4,10 +4,22 @@ export async function allshoes(req, res) {
 
     try {
         const lista = ((await db.query(`SELECT * FROM shoes WHERE "jaVendido"=$1`,[false])).rows);
-        console.log(lista)
         
         res.status(200).send(lista)
     } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function getbyid(req,res){
+    const {id} = req.params
+    try{
+        const produto = ((await db.query('SELECT shoes.*, users.cellnumber,users.email, users.name FROM shoes JOIN users ON users.id = shoes."userId" WHERE "userId"=$1',[id])).rows)[0]
+        
+        if (!produto) return res.sendStatus(404);
+        
+        res.send(produto)
+    }catch(err){
         res.status(500).send(err.message)
     }
 }
--- src/routers/shoes.routes.js
+++ src/routers/shoes.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { allshoes } from "../controllers/shoes.controller.js";
+import { allshoes, getbyid } from "../controllers/shoes.controller.js";
 
 
 const shoesRouter = Router();
 
 shoesRouter.get('/shoes' ,allshoes)
+shoesRouter.get('/shoes/:id',getbyid)
 
 export default shoesRouter
