86deff803810069770cb9f85f56d6fc399e1012a
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-08-14 18:35:25 -0300

    fix: to deploy

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -2,11 +2,10 @@ import { Router } from "express";
 import validateSchema from "../middlewares/validatSchemas.js";
 import { cadastroschema } from "../schemas/useonline.schema.js";
 import { signUP } from "../controllers/user.controller.js";
-import validateLogin from "../middlewares/validateLogin.js";
 
 
 const userRouter = Router();
 
-userRouter.post('/Signup', validateSchema(cadastroschema),signUP)
+userRouter.post('/SignUp', validateSchema(cadastroschema),signUP)
 
 export default userRouter
